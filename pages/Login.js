0855c7278369741ce8b57990189af3bc6d71e21f
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-01-07 16:58:43 +0700

    before commit

--- pages/Login.js
+++ pages/Login.js
@@ -137,7 +137,8 @@ export default class Login extends Component {
 
             <Button full bordered
               style={styles.btnForget}>
-                <Text uppercase={false} style={styles.btnForgetLabel}>Register</Text>
+                <Icon name='ios-lock' style={styles.btnForgetIcon}/>
+                <Text uppercase={false} style={styles.btnForgetLabel}>Forget Password</Text>
             </Button>
           </View>
         </View>
@@ -203,9 +204,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
     borderColor: '#3C40C5'
   },
+  btnForgetIcon: {
+    color:'#3C40C5',
+    fontSize: 16
+  },
   btnForgetLabel: {
     color: '#6c5ce7',
-    fontSize: 14
+    fontSize: 12,
+    marginLeft: -25
   },
   bgColor: {
     backgroundColor: '#C6FFDD',
