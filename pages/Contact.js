8b1e5ae414425f96b5b1fa07e0aced07f6a18519
Suwicha Saetie <suwicsae@METROSYSTEMS.CO.TH>
2019-01-10 16:12:59 +0700

    Update by mail

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Alert,AsyncStorage } from 'react-native';
+import { View, StyleSheet, Alert, AsyncStorage, StatusBar } from 'react-native';
 import { Container, Header, Left, Body, Right, Button, Icon, Title ,Input ,Item , Content, List, ListItem, Thumbnail, Text, rounded ,Spinner } from 'native-base';
 import Config from 'react-native-config'
+
 
 export default class Contact extends Component {
 
@@ -13,20 +14,8 @@ export default class Contact extends Component {
             empDataTemp : [],
             token: '',
             loading: false,
-            outerScrollEnable: true,
+            refreshing: false,
         };
-
-        verticalScroll = (index) => {
-            if(index !== 1 ){
-                this.setStaut({
-                outerScrollEnable: false
-                })
-            }else{
-                this.setStaut({
-                outerScrollEnable: true
-                })
-            }
-        }
     }
 
     componentWillMount() {
@@ -112,40 +101,49 @@ export default class Contact extends Component {
 
     listEmployee(empData) {
         return (empData.length === 0) ?  
-            <ListItem thumbnail style={styles.listitem}>
+            <ListItem circle thumbnail style={styles.listitem}>
                 <Body>
                     <Text note numberOfLines={1}>No Result</Text>
                 </Body>
             </ListItem>
         :
-            <List >
+            <List>
                 {   
                     empData.map(item => {
                         return ( 
-                            <ListItem thumbnail style={styles.listitem}>
+                            <ListItem circle thumbnail style={styles.listitem}
+                                // onScroll={event => { 
+                                //     // this.yOffset = event.nativeEvent.contentOffset.y
+                                //     console.log(event.nativeEvent.contentOffset.y);
+                                // }}
+                            >
                             <Left>
                                 <Thumbnail style={styles.imgbox} source={{ uri: item.imgPath }} />
                             </Left>
                             <Body style={{paddingTop: 5,paddingBottom: 5}}>
-                                <Text style={[styles.text1color]}>
+                                <Text note numberOfLines={1} style={[{ fontSize:16,fontWeight: 'bold' },styles.text1color]} > 
+                                { 
+                                    ( item.Phone3  != "") ?
+                                    " "+item.Phone3 : "     -"
+                                }  
+                                </Text> 
+                                <Text numberOfLines={1} style={[{ fontSize:14 },styles.text2color]} >
                                 {
                                     (item.NickName != "") ?
                                     "("+item.NickName+") ": ""
                                 }
                                 {item.FullName}
                                 </Text>
-                                <Text style={[{ fontSize:14 },styles.text2color]}> {item.email}</Text>
-                                
-                                    <Text note numberOfLines={1} style={[styles.text2color]} > 
-                                    { 
-                                        ( item.Phone3  != "") ?
-                                        "("+item.Phone3+") ": "     -"
-                                    }  
-                                    </Text>                              
+                                <Text style={[{ fontSize:14 },styles.text3color]}> {item.email} </Text>                            
                             </Body>
-                            <Right style={{marginRight: 14}} >
-                                <Button iconLeft transparent primary>
-                                    <Icon type="FontAwesome5" name="phone" style={{fontSize: 22, color: 'green'}} />
+                            <Right style={{marginRight: 14,padding:0}} >
+                                <Button iconLeft transparent primary
+                                    onPress={() => {
+                                                this.onClickCall(item.Phone3)
+                                            }
+                                        }
+                                    >
+                                    <Icon type="FontAwesome" name="phone" style={{fontSize: 22, color: 'green'}} />
                                 </Button>
                             </Right>
                             </ListItem>
@@ -155,45 +153,52 @@ export default class Contact extends Component {
             </List> 
     }
 
+    onClickCall = (Phone3) => {
+        Alert.alert(
+            'แจ้งเตือน',
+            'ต้องการโทรไปยังหมายเลข ' +Phone3+ ' หรือไม่',
+            [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+        )
+    }
+
     render() {
 
         return (
-
-        <Container>
-            <Header style={styles.header} >
-                <Left style={[{flex: 1}]}>
-                    <Button transparent onPress={() => {  this.props.navigation.navigate('Home'); }}>
-                        <Icon type="Ionicons" name='ios-arrow-back' style={styles.backbtn} />
-                    </Button>
-                </Left>
-                <Body style={[{flex: 9}]}>
-                    <Title style={[styles.textheader]}>Phone Book</Title>
-                </Body>
-                {/* <Right style={{backgroundColor: 'black'}}>
-                    <Button transparent>
-                    <Icon name='menu' />
-                    </Button>
-                </Right> */}
-            </Header>
-            <Content>
-                <View style={[styles.searchbox]}>
-                    <Item rounded style={[{ height: 40 },{backgroundColor: '#ececec'}]}>
-                        <Icon name="ios-search" style={[{marginLeft:7},styles.listicon]} />
-                        <Input 
-                        onChangeText = {(text) => this.onSearchChange(text)}
-                        placeholder="Search" style={{ fontSize:14}} />
-                    </Item>
-                </View>
-            {
-                (this.state.loading == false) ?
-                    <View style={[styles.loader]}>
-                        <Spinner color='blue' />
+            <Container>
+                <Header androidStatusBarColor="#3C40C5" style={{display:'none'}} style={[{backgroundColor: '#ececec'},styles.header]} >
+                    <Left style={[{flex: 1}]}>
+                        <Button transparent onPress={() => {  this.props.navigation.navigate('Home'); }}>
+                            <Icon type="Ionicons" name='ios-arrow-back' style={styles.backbtn} />
+                        </Button>
+                    </Left>
+                    <Body style={[{flex: 9,flexDirection:'row', flexWrap:'wrap',justifyContent:'flex-start'}]}>
+                        <Icon type="FontAwesome5" name='book' style={[styles.iconheader]} />
+                        <Title style={[styles.textheader]}>Phone Book</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <View style={[styles.searchbox]}>
+                        <Item rounded style={[{ height: 40 },{backgroundColor: '#ececec'}]}>
+                            <Icon name="ios-search" style={[{marginLeft:7},styles.listicon]} />
+                            <Input 
+                            onChangeText = {(text) => this.onSearchChange(text)}
+                            placeholder="Search" style={{ fontSize:14}} />
+                        </Item>
                     </View>
-                :
-                this.listEmployee(this.state.empData)
-            }
-            </Content>
-        </Container>
+                {
+                    (this.state.loading == false) ?
+                        <View style={[styles.loader]}>
+                            <Spinner color='blue' />
+                        </View>
+                    :
+                    this.listEmployee(this.state.empData)
+                }
+                </Content>
+            </Container>
         ); 
     }   
 }
@@ -209,11 +214,19 @@ const styles = StyleSheet.create({
         fontSize: 32,
     },
 
+    iconheader: {
+        // flex:1,
+        paddingTop:3,
+    },
+
     textheader: {
+        flex:0,
+        fontFamily: 'Source Sans Pro',
         color: '#000000', 
-        fontSize: 28, 
+        fontSize: 23, 
         fontWeight: 'bold',
         marginLeft: 0,
+        marginLeft: 12,
         // marginLeft: -22,
 
         // flex:1,
@@ -256,15 +269,20 @@ const styles = StyleSheet.create({
     },
 
     text1color: {
-        color: '#656262', 
+        fontFamily: 'Sarabun',
+        // color: '#656262', 
+
+        color: "#2b30db",
     },
 
     text2color: {
-        color: '#AAAAAA', 
+        fontFamily: 'Sarabun',
+        color: '#37373a', 
     },
 
     text3color: {
-        color: '#DDDDDD', 
+        fontFamily: 'Source Sans Pro',
+        color: '#949496', 
     },
 
 });
